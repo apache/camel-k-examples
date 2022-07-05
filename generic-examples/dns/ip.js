b29c169fc94869ad95a40bf8e56d9d79b85ea1cc
Kuthumi Pepple <kuthumipepple@gmail.com>
2022-07-05 08:11:42 +0100

    rename dns.js to ip.js

--- generic-examples/dns/ip.js
+++ generic-examples/dns/ip.js
@@ -17,16 +17,12 @@
 //
 // To run this integrations use:
 //
-//     kamel run -d camel-dns examples/dns.js
-//
-// Or simply (since dependency auto-detection is enabled by default):
-//
-//     kamel run examples/dns.js
+//     kamel run ip.js
 //
 
 from('timer:dns?period=1000')
-    .routeId('dns')
-    .setHeader('dns.domain')
-        .constant('www.google.com')
-    .to('dns:ip')
-    .to('log:dns');
+  .routeId('dns')
+  .setHeader('dns.domain')
+  .constant('www.google.com')
+  .to('dns:ip')
+  .to('log:dns');
